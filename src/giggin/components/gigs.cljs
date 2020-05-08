fab1bdfb535ab726df7809902526cf4c9cbfa71c
Kaikaine <kairijb@protonmail.com>
2020-05-07 23:24:11 -0500

    price formatted correctly

--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -1,5 +1,6 @@
 (ns giggin.components.gigs
-  (:require [giggin.state :as state]))
+  (:require [giggin.state :as state]
+            [giggin.helpers :refer [format-price]]))
 
 (defn gigs
   []
@@ -14,7 +15,7 @@
           {:data-tooltip "Add to order"
            :on-click (fn [] (swap! state/orders update id inc))}
           [:i.icon.icon--plus]] title]
-        [:p.gig__price price]
+        [:p.gig__price (format-price price)]
         [:p.gig__desc desc]]])]])
 
 ; (defn gigs
